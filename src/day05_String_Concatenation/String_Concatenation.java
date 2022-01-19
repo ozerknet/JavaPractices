b94bf40dd1bbc437e1ca0097123c28f1f1b6aba9
ozerknet <ozerknet@gmail.com>
2022-01-19 11:11:05 +0000

    Create String_Concatenation.java

---
+++ src/day05_String_Concatenation/String_Concatenation.java
@@ -0,0 +1,21 @@
+package day05_String_Concatenation;
+
+// soutv --> print variable
+public class String_Concatenation {
+
+    byte num1 = 100;
+    short num2 = 10000;
+    double num3 = 3.5;
+    float num4 = 2.5f;
+    long num5 = 99999999999l;
+    int num6 = 850000;
+
+        System.out.println("num1 = "+num1);
+        System.out.println("num2 = "+num2);
+        System.out.println("num3 = "+num3);
+        System.out.println("num4 = "+num4);
+        System.out.println("num5 = "+num5);
+        System.out.println("num6 = "+num6);
+
+
+}
