0928183106d461ae1884aa9fce15eb51561281df
ozerknet <ozerknet@gmail.com>
2022-01-19 11:46:37 +0000

    Day05

--- src/day05_String_Concatenation/String_Concatenation.java
+++ src/day05_String_Concatenation/String_Concatenation.java
@@ -1,14 +1,15 @@
 package day05_String_Concatenation;
 
-// soutv --> print variable
 public class String_Concatenation {
 
-    byte num1 = 100;
-    short num2 = 10000;
-    double num3 = 3.5;
-    float num4 = 2.5f;
-    long num5 = 99999999999l;
-    int num6 = 850000;
+    public static void main(String[] args) {
+        /*
+        byte num1 = 100;
+        short num2 = 10000;
+        double num3 = 3.5;
+        float num4 = 2.5f;
+        long num5 = 99999999999l;
+        int num6 = 850000;
 
         System.out.println("num1 = "+num1);
         System.out.println("num2 = "+num2);
@@ -17,5 +18,27 @@ public class String_Concatenation {
         System.out.println("num5 = "+num5);
         System.out.println("num6 = "+num6);
 
+        String n1 = "Number 1" ;
+
+        System.out.println(n1 + " : " + num1);
+
+       */
+        /*
+        String Hi = "Hi, my name is ";
+        String name = "Ozer";
+
+        String age_sentences = " and I am ";
+        int age = 38;
+        String end = " years old.";
+
+        System.out.println(Hi + name + age_sentences + age + end);
+*/
+
+        String name = "Mike";
+        int age = 50 ;
+        System.out.println("Hello Everyone. My name is " + name + " and I am " + age + " years old.");
+
+
+    }
 
 }
