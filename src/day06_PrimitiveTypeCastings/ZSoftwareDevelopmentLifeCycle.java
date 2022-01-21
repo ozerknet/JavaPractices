b1f515f18912a41f2b2945e3713e384bfd357a6f
ozerknet <ozerknet@gmail.com>
2022-01-21 11:28:36 +0000

    21 January 2022

---
+++ src/day06_PrimitiveTypeCastings/ZSoftwareDevelopmentLifeCycle.java
@@ -0,0 +1,34 @@
+package day06_PrimitiveTypeCastings;
+
+public class ZSoftwareDevelopmentLifeCycle {
+    public static void main(String[] args) {
+        System.out.println("---------------------");
+        System.out.println("S D L C Steps");
+        System.out.println("---------------------");
+        String step1 = "Requirement Gathering";
+        String step2 = "Design";
+        String step3 = "Develop";
+        String step4 = "Test";
+        String step5 = "Release";
+        String step6 = "Maintenance";
+        System.out.println(step1 + "\n\t" +
+                step2 + "\n\t\t" +
+                step3 + "\n\t" +
+                "\'" + step4 + "\'" + "\n" +
+                step5 +
+                "\n\t\t\t\t" + step6);
+
+String development = "dev.domain.com";
+String test = "test.domain.com";
+String staging = "domain.com";
+String production = "domain.com";
+
+
+        System.out.println("List of Environments");
+        System.out.println("1. development = " + development);
+        System.out.println("2. test = " + test);
+        System.out.println("3. staging = " + staging);
+        System.out.println("3. production = " + production);
+
+    }
+}
