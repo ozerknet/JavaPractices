8f7d6da0e290e5c658502797f6dbe0fbf515a067
ozerknet <ozerknet@gmail.com>
2022-01-25 10:30:25 +0000

    25 January 22

--- src/day07_Operators/ZPrac3.java
+++ src/day07_Operators/ZPrac3.java
@@ -3,5 +3,39 @@ package day07_Operators;
 public class ZPrac3 {
     public static void main(String[] args) {
 
+        /*
+
+
+        boolean result1 = 100 <= 99;
+
+        System.out.println("result1 = " + result1);
+
+        // credit score of 720;
+
+        int creditScore = 101;
+        boolean isEligibleForLoan = creditScore >= 100; // IF THE CREDIT SCORE IS 100 OR MORE, THEN IT IS ELIGIBLE FOR LOAN
+
+        System.out.println("isEligibleForLoan = " + isEligibleForLoan);
+
+        int num1 = 5;
+        int num2 = 5;
+
+        boolean b = num1 >= num2;
+        System.out.println("b = " + b);
+
+        */
+
+        boolean result7 = 'A' == 'a';
+        System.out.println("result7 = " + result7);
+
+
+        boolean result9 = "Java" ==  "Java" ;  // true
+        System.out.println("---result9 = " + result9);
+        boolean result10 = "Java" ==  "Java   " ;  // false
+        System.out.println("result10 = " + result10);
+
+        boolean result11 = "Ozer" != "Ozer " ;
+        System.out.println("------> result11 = " + result11);
+
     }
 }
