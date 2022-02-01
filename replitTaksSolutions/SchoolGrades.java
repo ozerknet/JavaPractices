22f7f3741765a968bf61769ebc43fc55835ba434
ozerknet <ozerknet@gmail.com>
2022-02-01 18:51:14 +0000

    day11

---
+++ replitTaksSolutions/SchoolGrades.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SchoolGrades {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Grader!");
+        System.out.println("Please enter subject name number 1 and score for this subject ");
+
+
+
+    }
+}
+
+
+/*
+Write the program that will calculate the average grade.
+Use 5 String variables and 6 double variables to hold the subject and the grade value.
+Build a summary with all the information by concatenating the subjects and grades.
+Then calculate the average grade and print display the average score too.
+
+Execution flow with example:
+
+Welcome to the Grader!
+Please enter subject name number 1 and score for this subject
+Math
+5.0
+Please enter subject name number 2 and score for this subject
+Biology
+4.2
+Please enter subject name number 3 and score for this subject
+English
+4.4
+Please enter subject name number 4 and score for this subject
+Chemistry
+4.8
+Please enter subject name number 5 and score for this subject
+Music
+3.4
+​
+Summary: Math - 5.0,  Biology - 4.2,  English - 4.4, Chemistry - 4.8, Music - 3.4
+Your average score is: 4.36
+Thank you for using Grader!
+Goodbye!
+* */
