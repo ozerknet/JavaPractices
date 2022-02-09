9490da8c758c89e31d20767608cc550616945f93
ozerknet <ozerknet@gmail.com>
2022-02-09 18:27:47 +0000

    day14 practice

---
+++ src/day14_String/ZPrac6.java
@@ -0,0 +1,19 @@
+package day14_String;
+
+public class ZPrac6 {
+    public static void main(String[] args) {
+        String str = "Cydeo";
+
+        String s1 = str.repeat(4);
+     
+        System.out.println(s1);
+        
+        String str1 = "Java java";
+        
+        String s2 = str1.repeat(100);
+
+        System.out.println("s2 = " + s2);
+
+
+    }
+}
