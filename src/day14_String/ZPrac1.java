e197830511a6a330542a60b4d866e590ee3769ad
ozerknet <ozerknet@gmail.com>
2022-02-09 11:09:26 +0000

    day14

---
+++ src/day14_String/ZPrac1.java
@@ -0,0 +1,18 @@
+package day14_String;
+
+public class ZPrac1 {
+    public static void main(String[] args) {
+        String s = " Java CAva Wawa orange";
+        int second = s.indexOf("av");
+        int foruthA = s.lastIndexOf("av")+1;
+        int fifthA = s.lastIndexOf("a W");
+
+        System.out.println("second = " + second);
+        System.out.println("foruthA = " + foruthA);
+        System.out.println("fifth = " + fifthA);
+
+        System.out.println("------------------------------------------------------");
+
+        
+    }
+}
