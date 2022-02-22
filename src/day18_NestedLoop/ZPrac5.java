8a9478ab5b07f5ae176595be9d379ad72a76bdfb
ozerknet <ozerknet@gmail.com>
2022-02-22 22:31:16 +0000

    Study this example

---
+++ src/day18_NestedLoop/ZPrac5.java
@@ -0,0 +1,19 @@
+package day18_NestedLoop;
+
+public class ZPrac5 {
+    public static void main(String[] args) {
+
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            result = "*"+i;
+
+            for (int k = 0; k < 10; k++) {
+
+            }
+            System.out.println();
+            System.out.println(result);
+        }
+
+
+    }
+}
