ce4192d656f9c60b080babaa5995a038d9b57a4f
ozerknet <ozerknet@gmail.com>
2022-02-23 22:19:25 +0000

    Practice 23/2/2022

---
+++ src/day18_NestedLoop/ZPraac2Fibonacci.java
@@ -0,0 +1,24 @@
+package day18_NestedLoop;
+
+public class ZPraac2Fibonacci {
+    public static void main(String[] args) {
+
+        int a=0;
+        int b=1;
+
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+}
+
+
+/*
+
+0,1,1,2,3,5,8,
+
+
+
+
+
+*/
--- src/day18_NestedLoop/ZPrac5.java
+++ src/day18_NestedLoop/ZPrac5.java
@@ -3,17 +3,31 @@ package day18_NestedLoop;
 public class ZPrac5 {
     public static void main(String[] args) {
 
-        String result = "";
+//        String result = "";
+//        for (int i = 0; i < 10; i++) {
+//            result = "*"+i;
+//
+//            for (int k = 0; k < 10; k++) {
+//
+//            }
+//            System.out.println();
+//            System.out.println(result);
+        int a;
+        int b;
+
         for (int i = 0; i < 10; i++) {
-            result = "*"+i;
-
-            for (int k = 0; k < 10; k++) {
-
+            System.out.println("Ramazan");
+            for (int j = 0; j < 10; j++) {
+                System.out.print("* ");
+                if ((i == j)) {
+                    break;
+                }
             }
-            System.out.println();
-            System.out.println(result);
         }
 
 
     }
+
+
 }
+
