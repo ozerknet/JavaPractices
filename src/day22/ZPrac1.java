455e78e8556bf025c9322b67a34e5219a0e7405a
ozerknet <ozerknet@gmail.com>
2022-03-07 12:14:47 +0000

    Practice lastweek

---
+++ src/day22/ZPrac1.java
@@ -0,0 +1,20 @@
+package day22;
+
+import java.util.Arrays;
+
+public class ZPrac1 {
+    public static void main(String[] args) {
+        String sentence = "Today is a good day to learn Java";
+
+        String[] words = sentence.split(" ");
+
+        System.out.println(Arrays.toString(words));
+        String reversedSentence = "";
+
+        for (int i = words.length - 1; i >=0 ; i--) {
+            reversedSentence += words[i] + " ";
+        }
+
+        System.out.println(reversedSentence);
+    }
+}
