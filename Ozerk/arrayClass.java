7ed9c259ab28267090cdc3ed6aebdf62ba9845f0
Ozer <ozerknet@gmail.com>
2022-04-01 20:06:35 +0100

    Updated

---
+++ Ozerk/arrayClass.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrayClass {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] nums = {input.nextInt(),input.nextInt()};
+        System.out.println(Arrays.toString(nums));
+    }
+}
