229aa54321c8ebdcff9d81353bbb34a15ffea6a2
Ozer <ozerknet@gmail.com>
2022-04-12 17:34:37 +0100

    Update

--- src/day38_Inheritance/MyCarTask/Garage.java
+++ src/day38_Inheritance/MyCarTask/Garage.java
@@ -3,9 +3,13 @@ package day38_Inheritance.MyCarTask;
 public class Garage {
     public static void main(String[] args) {
         Bmw BW1 = new Bmw("X5", 2006, 12000, "red", 140000);
+        Tesla Tes1 = new Tesla("Z",2002,152000,"brown",25000);
+        Toyota Toy1 = new Toyota("Auris", 2010, 42000, "black", 440000);
 
 
-        System.out.println("BW1 = " + BW1);
+        System.out.println("Bmw = " + BW1);
+        System.out.println("Tesla = " + Tes1);
+        System.out.println("Toyota = " + Toy1);
 
         BW1.start();
         BW1.racing();
