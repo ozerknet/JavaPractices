af5ba5808133ab02dda59e9423fac77f9b33dede
Ozer <ozerknet@gmail.com>
2022-04-13 15:10:24 +0100

    School Project start with teacher class added

---
+++ src/day39_Recap/mySchool/Teacher.java
@@ -0,0 +1,16 @@
+package day39_Recap.mySchool;
+
+public class Teacher extends Employee {
+    public Teacher(String name, String surName, int age, char gender, String employeeId, String jobTitle, int salary) {
+        super(name, surName, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle() + " " + getName() + " is creating ticket.");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " "+ getName() + " is testing the application.");
+    }
+}
