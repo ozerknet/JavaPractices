e2ba887aedb84f01fadc503df3a09763c395cc15
ozerknet <ozerknet@gmail.com>
2022-04-17 00:44:15 +0100

    updated

---
+++ src/day40_FinalKeyword/Ramazan/AreaManager.java
@@ -0,0 +1,8 @@
+package day40_FinalKeyword.Ramazan;
+
+public class AreaManager extends Ramazan {
+    public AreaManager() {
+        super();
+
+    }
+}
---
+++ src/day40_FinalKeyword/Ramazan/Trustee.java
@@ -0,0 +1,17 @@
+package day40_FinalKeyword.Ramazan;
+
+public class Trustee extends AreaManager {
+
+    int NumberOfPersonMakingIftar;
+    String IftarNameOfPlace;
+
+    public Trustee(int numberOfPersonMakingIftar, String iftarNameOfPlace) {
+        NumberOfPersonMakingIftar = numberOfPersonMakingIftar;
+        IftarNameOfPlace = iftarNameOfPlace;
+    }
+
+    public void iftar(String TrusteeName, int numberOfPersonMakingIftar, String IftarNameOfPlace) {
+        System.out.println(TrusteeName + " is invited " + numberOfPersonMakingIftar + "at" + IftarNameOfPlace);
+    }
+
+}
