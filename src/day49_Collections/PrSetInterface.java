c36db2d8f0f10a450750efa8d989058c266da735
Ozer <ozerknet@gmail.com>
2022-05-17 12:53:55 +0100

    PracticeCollections

---
+++ src/day49_Collections/PrSetInterface.java
@@ -0,0 +1,26 @@
+package day49_Collections;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class PrSetInterface {
+    public static void main(String[] args) {
+
+
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(300, 449, 657, 12341, 123, 1231));
+        System.out.println(hashSet);
+
+
+
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+
+
+
+
+
+        Set<Integer> treeSet = new TreeSet<>();
+
+
+    }
+}
